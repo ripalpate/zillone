0b5791bc9e0f686b683f33a4c864c6dcda09026d
Ripal Patel <ripal212007@gmail.com>
2018-12-18 19:48:11 -0600

    address value is changing

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -1,26 +1,65 @@
 import React from 'react';
 import './ListingForm.scss';
 
+const defaultListing = {
+  address: '',
+  squareFootage: 0,
+  price: 0,
+  numBeds: 0,
+  numBaths: 0,
+  heating: '',
+  cooling: '',
+  imageUrl: '',
+  uid: '',
+};
+
 class ListingForm extends React.Component {
-  render() {
-    return (
+  state = {
+    newListing: defaultListing,
+  }
+  // Spread Operator
+  // const a = [1,2,3];
+  // const b = [4,5,6];
+  // const big = [...this.a, ...this.b]
+  // output [1,2,3,4,5,6]
+
+formFieldStringState = (name, e) => {
+  e.preventDefault();
+  const tempListing = { ...this.state.newListing };
+  tempListing[name] = e.target.value;
+  this.setState({ newListing: tempListing });
+}
+
+// formFieldNumberState = (name, e) => {
+//   const tempListing = { ...this.state.newListing };
+//   tempListing[name] = e.target.value * 1;
+//   this.setState({ newListing: tempListing });
+// }
+
+addressChange = e => this.formFieldStringState('address', e);
+
+render() {
+  const { newListing } = this.state;
+  return (
       <div className="listing-form col">
         <h2>Add New Listing:</h2>
         <form>
           <div className="form-group">
-            <label for="address">Address</label>
-            <input 
-              type="text" 
-              className="form-control" 
-              id="address" 
-              aria-describedby="addressHelp" 
-              placeholder="123 Main Street, Nashville, TN 37209"/>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              aria-describedby="addressHelp"
+              placeholder="123 Main Street, Nashville, TN 37209"
+              value = { newListing.address }
+              onChange = {this.addressChange}/>
           </div>
           <button className="btn btn-danger">Save Listing</button>
         </form>
       </div>
-    );
-  }
+  );
+}
 }
 
 export default ListingForm;
